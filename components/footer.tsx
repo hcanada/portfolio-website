0959b2fc78604872d09125af65ffd4ee13b47626
xerea <harlybusinessmind@gmail.com>
2025-12-10 17:35:22 +0800

    added a footer

---
+++ components/footer.tsx
@@ -0,0 +1,10 @@
+export const Footer = () => {
+  return (
+    <footer className="w-full text-center pb-10 mt-25  ">
+      <p>
+        © 2025 H<span className="text-primary font-semibold">Canada</span>. All
+        rights reserved.
+      </p>
+    </footer>
+  );
+};
