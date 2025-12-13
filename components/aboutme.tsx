c66ec81f97a585e2d85a922063759dbfc5257080
xerea <harlybusinessmind@gmail.com>
2025-12-13 14:59:27 +0800

    added about me section

---
+++ components/aboutme.tsx
@@ -0,0 +1,27 @@
+const Aboutme = () => {
+  return (
+    <div className="mx-auto w-full max-w-7xl px-4 sm:px-6 lg:px-8 mt-24 md:mt-28 lg:mt-32 2xl:mt-36 gap-16 scroll-mt-16 mb-50 text-center ">
+      <h1 className="font-bold text-5xl mb-10 md:mb-15">About me</h1>
+      <p className="leading-7 md:px-50">
+        I&apos;m a full-stack software engineer specializing in developing
+        solutions with JavaScript, Python, and PHP. I work on projects including
+        building modern web applications, mobile apps, search engine
+        optimization, digital marketing, and making code tutorials..
+        <br /> <br />
+        I&apos;ve helped startups and MSMEs grow and streamline their processes
+        through software solutions. I&apos;ve also built a community of over
+        200,000 developers sharing knowledge and mentorship.
+        <br />
+        <br />
+        Lately, I&apos;ve been diving deeper into the world of artificial
+        intelligence, focusing on integrating AI tools and techniques into
+        modern applications. My work now includes developing AI-powered
+        solutions, creating intelligent applications, and leveraging generative
+        AI to optimize development workflows and deliver cutting-edge
+        technology.
+      </p>
+    </div>
+  );
+};
+
+export default Aboutme;
