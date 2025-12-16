c131d5a755080a6c45e42d5a73af8e4cf5f8f4c9
xerea <harlybusinessmind@gmail.com>
2025-12-16 21:42:35 +0800

    feat: update Aboutme component with detailed professional description

--- components/aboutme.tsx
+++ components/aboutme.tsx
@@ -3,11 +3,16 @@ const Aboutme = () => {
     <div className="mx-auto w-full max-w-7xl px-4 sm:px-6 lg:px-8 mt-24 md:mt-28 lg:mt-32 2xl:mt-36 gap-16 scroll-mt-16 text-center ">
       <h1 className="font-bold text-5xl mb-10 md:mb-15">About me</h1>
       <p className="leading-7 px-5 sm:px-15 md:px-30 lg:px-50 text-pretty">
-        I’m a web developer who enjoys building clean and functional web
-        applications. I’ve worked on real-world systems, including a Leave
-        Management System used in a corporate environment. Currently, I focus on
-        Next.js, Node.js (Express), and PostgreSQL while building and learning
-        through projects.
+        I’m a Full-stack Developer with experience building reliable and user
+        friendly web applications. I work across both frontend and backend
+        systems to deliver solutions that are clean, efficient, and easy to
+        maintain. <br />
+        <br /> I focus on writing quality code, improving performance, and
+        creating smooth user experiences. I enjoy turning complex requirements
+        into simple, practical solutions and building applications that are
+        scalable and well structured. I value clear communication, meeting
+        deadlines, and understanding project goals to ensure the final product
+        meets both technical and business needs.
         <br />
         <br />
         Outside of coding, I explore new technologies and follow web development
