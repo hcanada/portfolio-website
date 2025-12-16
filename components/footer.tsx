b895ed26e4094cb9c5702a3812a02531dbe69ca6
xerea <harlybusinessmind@gmail.com>
2025-12-16 17:10:43 +0800

    update footer text to remove copyright notice

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,8 +2,7 @@ export const Footer = () => {
   return (
     <footer className="w-full text-center pb-10 mt-25  ">
       <p>
-        © 2025 H<span className="text-primary font-semibold">Canada</span>. All
-        rights reserved.
+        2025 © H<span className="text-primary font-semibold">Canada</span>
       </p>
     </footer>
   );
